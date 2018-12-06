ed7d775e233a5c1b2531019e622e0f6dfa96f69b
liminjun <v-leeli@hotmail.com>
2018-12-06 21:22:18 +0800

    feat:React Animation Components

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -2,19 +2,25 @@ import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
-import {Loading} from './LoadingComponent';
+import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
+
 
 function RenderDish({ dish }) {
   if (dish) {
     return (
-      <Card>
-        <CardImg src={baseUrl + dish.image} alt={dish.name}></CardImg>
-        <CardBody>
-          <CardTitle>{dish.name}</CardTitle>
-          <CardText>{dish.description}</CardText>
-        </CardBody>
-      </Card>
+      <FadeTransform in transformProps={{
+        exitTransform: 'scale(0.5) translateY(-50%)'
+      }}>
+        <Card>
+          <CardImg src={baseUrl + dish.image} alt={dish.name}></CardImg>
+          <CardBody>
+            <CardTitle>{dish.name}</CardTitle>
+            <CardText>{dish.description}</CardText>
+          </CardBody>
+        </Card>
+      </FadeTransform>
 
 
     );
@@ -28,19 +34,25 @@ function RenderDish({ dish }) {
 function RenderComments({ comments, postComment, dishId }) {
 
   if (comments) {
-    const commentList = comments.map((comment) => {
-      return (
-        <li key='' className="col-12 col-md-12 m-1">
-          <div>{comment.content} </div>
-          <div>--{comment.author},{new Intl.DateTimeFormat('zh-CN', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</div>
-        </li>
-      );
-    });
+
     return (
       <div>
         <h1>Comments</h1>
         <ul>
-          {commentList}
+          <Stagger in>
+            {
+              comments.map((comment) => {
+                return (
+                  <Fade in>
+                    <li key='' className="col-12 col-md-12 m-1">
+                      <div>{comment.content} </div>
+                      <div>--{comment.author},{new Intl.DateTimeFormat('zh-CN', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</div>
+                    </li>
+                  </Fade>
+                );
+              })
+            }
+          </Stagger>
         </ul>
       </div>
     )
